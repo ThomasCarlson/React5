c8895bf079624cca715a829e011cf344d0951b37
Thomas Carlson <tcarlson@onedoor.com>
2020-04-18 13:24:04 -0400

    add npm scripts, eslint, Prettier, gitignore

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-const Pet = props => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed)
-  ]);
-};
+import React from 'react';
+// import ReactDOM from 'react-dom';
+import { render } from 'react-dom';  // this will import ONLY render from the package
+import Pet from "./Pet";
 
 const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, "Adopt Me!"),
     React.createElement(Pet, {
-      name: "Luna",
+      name: "Maggie",
       animal: "Dog",
-      breed: "Havanese"
+      breed: "Lab"
     }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel"
-    }),
-    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" })
+    React.createElement(Pet, { name: "Misty", animal: "Cat", breed: "Mix" })
   ]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+// ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(React.createElement(App), document.getElementById("root"));
